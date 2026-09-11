3e8aaad5da1046d9e41b701ffe9afa7d0d38994c
VICENTE ALFARO <vgalfaro1@uc.cl>
2026-09-11 17:32:33 -0300

    calculo mu y sigma muestrales

---
+++ scr/returns_from_prices.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+TRADING_DAYS = 252
+
+data = pd.read_csv('close-values-1mo.csv', index_col=0, parse_dates=True)
+returns = data.pct_change().dropna()
+
+# annualized mu and sigma
+mu: pd.DataFrame= returns.mean() * TRADING_DAYS
+sigma: pd.DataFrame = returns.cov() * TRADING_DAYS
+
+print(type(mu), type(sigma))
